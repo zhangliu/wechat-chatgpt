7b526bc642e405caa3d7092a289d62b810ca8824
zhangyunnian <zhangyunnian@bytedance.com>
2023-04-03 21:02:20 +0800

    modify

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,51 @@
-import { WechatyBuilder } from "wechaty";
-import QRCode from "qrcode";
-import { ChatGPTBot } from "./bot.js";
-import {config} from "./config.js";
+import { WechatyBuilder } from 'wechaty';
+import QRCode from 'qrcode';
+import { ChatGPTBot } from './bot.js';
+import { config } from './config.js';
 const chatGPTBot = new ChatGPTBot();
 
-const bot =  WechatyBuilder.build({
-  name: "wechat-assistant", // generate xxxx.memory-card.json and save login data for the next login
-  puppet: "wechaty-puppet-wechat",
+const bot = WechatyBuilder.build({
+  name: 'wechat-assistant', // generate xxxx.memory-card.json and save login data for the next login
+  puppet: 'wechaty-puppet-wechat',
   puppetOptions: {
-    uos: true
-  }
+    uos: true,
+  },
 });
 async function main() {
-  const initializedAt = Date.now()
+  const initializedAt = Date.now();
   bot
-    .on("scan", async (qrcode, status) => {
+    .on('scan', async (qrcode, status) => {
       const url = `https://wechaty.js.org/qrcode/${encodeURIComponent(qrcode)}`;
       console.log(`Scan QR Code to login: ${status}\n${url}`);
       console.log(
-        await QRCode.toString(qrcode, { type: "terminal", small: true })
+        await QRCode.toString(qrcode, { type: 'terminal', small: true })
       );
     })
-    .on("login", async (user) => {
+    .on('login', async (user) => {
       chatGPTBot.setBotName(user.name());
       console.log(`User ${user} logged in`);
       console.log(`私聊触发关键词: ${config.chatPrivateTriggerKeyword}`);
-      console.log(`已设置 ${config.blockWords.length} 个聊天关键词屏蔽. ${config.blockWords}`);
-      console.log(`已设置 ${config.chatgptBlockWords.length} 个ChatGPT回复关键词屏蔽. ${config.chatgptBlockWords}`);
+      console.log(
+        `已设置 ${config.blockWords.length} 个聊天关键词屏蔽. ${config.blockWords}`
+      );
+      console.log(
+        `已设置 ${config.chatgptBlockWords.length} 个ChatGPT回复关键词屏蔽. ${config.chatgptBlockWords}`
+      );
     })
-    .on("message", async (message) => {
+    .on('message', async (message) => {
       if (message.date().getTime() < initializedAt) {
         return;
       }
-      if (message.text().startsWith("/ping")) {
-        await message.say("pong");
+      if (message.text().startsWith('/ping')) {
+        await message.say('pong');
         return;
       }
+      const text = message.text();
+      const type = message.type();
+      console.log('text:', text, 'type:', type, 'xxxxxx');
+      if (+type !== 7) return;
+      if (!text.startsWith('/gpt')) return;
+
       try {
         await chatGPTBot.onMessage(message);
       } catch (e) {
